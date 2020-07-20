b007194dbe4c1ed468278ff95a7bfe6fca22b4d0
Marc Midura <marc.midura@gmail.com>
2020-07-20 15:52:06 -0600

    Added player control to move left/right.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        float xpos = Mathf.Clamp(transform.position.x, -xRange, xRange);
+        transform.position = new Vector3(xpos, transform.position.y, transform.position.z);
+    }
+}
