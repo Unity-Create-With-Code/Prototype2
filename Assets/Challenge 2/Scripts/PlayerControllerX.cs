641abf697faaf89597efb612359e88f15e19ce8e
Marc Midura <mmidura@diamondvisionics.com>
2020-07-21 15:44:34 -0600

    Limit dog spawning.
    Enforce a delay before the player is allowed to spawn a new dog.

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
-    
+    public float waitTimeToSpawnDog = 2.0f;
+    private float elapsedDogSpawnTime = 0.0f;
+
     // Update is called once per frame
     void Update()
     {
+        elapsedDogSpawnTime += Time.deltaTime;
+
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (elapsedDogSpawnTime > waitTimeToSpawnDog && Input.GetKeyDown(KeyCode.Space))
         {
+            elapsedDogSpawnTime = 0.0f;
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
         }
     }
